b8edd31e1b8f3e3823f35610554360dff8b4dd1c
Oshioyemi Ridwan <rhidone01@gmail.com>
2022-11-06 18:38:10 +0100

    Modified group_anagram.py

--- group_anagrams.py
+++ group_anagrams.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 def group_anagram(a):
     dfdict = defaultdict(list)
     for i in a:
-        sorted_word = ''.join((i))
+        sorted_word = ''.join(sorted(i))
         dfdict[sorted_word].append(i)
     return dfdict.values()
+b = 'ant','bat','ten','tab','net'
+print(group_anagram(b))
